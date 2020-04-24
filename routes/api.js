4e80690c15a98e11de186830d0ff41c3cb0941e9
Leo La <leola90@gmail.com>
2020-04-24 10:10:47 -0500

    added api routes

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
+const Exercise = require("../models/exercise.js");
+
+router.post("/api/exercise", ({ body }, res) => {
+    Exercise.create(body)
+    .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
+router.get("/api/exercise", (req, res) => {
+    Exercise.find({})
+      .sort({ date: -1 })
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
 module.exports = router;
