8d07a923ba7ae7c6f8ba175bf5d46001d00a1889
Leo La <leola90@gmail.com>
2020-04-25 22:18:27 -0500

    updated server.js to include new routes

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+  useNewUrlParser: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
+require("./routes/api-route.js")(app);
+require("./routes/html-route.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
