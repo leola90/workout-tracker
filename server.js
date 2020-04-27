e4ec7eb5ab02dc1a5c8d0249c6797b68bd520959
Leo La <leola90@gmail.com>
2020-04-26 23:39:10 -0500

    updated server.js and package.json

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 require("./routes/api-route.js")(app);
